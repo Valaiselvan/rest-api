372843a3c6787350f2c7c573d7b31135fd63cf50
Valaiselvan <selvan0606@gmail.com>
2025-10-14 16:13:01 -0400

    email send

--- news.py
+++ news.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "f178f2ad6e35440eb18d2779a0c7f416"
 url = ("https://newsapi.org/v2/everything?q=tesla&from=2025-09-14&"
@@ -7,10 +8,15 @@ url = ("https://newsapi.org/v2/everything?q=tesla&from=2025-09-14&"
 # Make Request
 request = requests.get(url, verify=False)
 
-# Get a dictonary with data
+# Get a dictionary with data
 content = request.json()
 
 # Access the article titles and description
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+   if article["title"] is not None:
+       body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body  = body.encode("utf-8")
+send_email(message=body)
+
