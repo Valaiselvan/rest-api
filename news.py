a6252d1a3e90261b04fbeeb9b1ded9d8eda94297
Valaiselvan <selvan0606@gmail.com>
2025-10-14 21:05:12 -0400

    email send corrections

--- news.py
+++ news.py
@@ -1,9 +1,13 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
+
 api_key = "f178f2ad6e35440eb18d2779a0c7f416"
-url = ("https://newsapi.org/v2/everything?q=tesla&from=2025-09-14&"
-       "sortBy=publishedAt&apiKey=f178f2ad6e35440eb18d2779a0c7f416")
+url = ("https://newsapi.org/v2/everything?"\
+       f"q={topic}&sortBy=publishedAt"\
+       f"&apiKey=f178f2ad6e35440eb18d2779a0c7f416&"\
+       "language=en")
 
 # Make Request
 request = requests.get(url, verify=False)
@@ -13,9 +17,12 @@ content = request.json()
 
 # Access the article titles and description
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:20]:
    if article["title"] is not None:
-       body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+       body = "Subject: Today's news"\
+               +"\n" + body + article["title"] + "\n" \
+               + article["description"] \
+               + "\n" + article["url"] + 2*"\n"
 
 body  = body.encode("utf-8")
 send_email(message=body)
